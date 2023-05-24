7e9e4ed50bd45dd35bf49b489366c65cf1f1fbce
Peterhabit <jiseokchoij@gmail.com>
2023-05-24 12:51:06 +0900

    백업

--- src/main/java/idusw/springboot/domain/PageResultDTO.java
+++ src/main/java/idusw/springboot/domain/PageResultDTO.java
@@ -11,28 +11,40 @@ import java.util.stream.IntStream;
 
 @Data //5가지 애노테이션 묶어놓은 것
 public class PageResultDTO<DTO, EN> {
-    private List<DTO> dtoList;
+    private List<DTO> dtoList; //결과를 담고 있다
 
     private int totalPage; // 총 페이지 번호 = (총 레코드수 / 한 페이지 게시물 갯수 = 값을 올림해줌), 나머지가 있다면 + 1
     private int curPage; // 현재 페이지 번호
     private int size; // 한 페이지 게시물 갯수
+    private int perPage; // 한 페이지 게시물(레코드 갯수)
+    private int perPagination; // 한 페이지의 페이지 번호 갯수
+    private long totalRows; //총 갯수
+    private int startRow, endRow; //시작 레코드 번호, 끝 레코드 번호
 
     private int start, end; // 시작 페이지 번호, 끝 페이지 번호
     private boolean prev, next; // 버튼 표시
 
     private List<Integer> pageList; // 페이지 번호 목록
 
-    public  PageResultDTO(Page<EN> result, Function<EN,DTO>fn){
+    public  PageResultDTO(Page<EN> result, Function<EN,DTO>fn, int perPagination){
+        totalRows = result.getTotalElements();
         dtoList = result.stream().map(fn).collect(Collectors.toList()); // get records
         totalPage = result.getTotalPages();
+        this.perPagination = perPagination;
         makePageList(result.getPageable());
     }
 
     private void makePageList(Pageable pageable) {
-        this.curPage = pageable.getPageNumber() + 1;
-        this.size = pageable.getPageSize();
-        int tempEnd = (int)(Math.ceil(curPage / 3.0)) * 3;
-        start = tempEnd - 3 + 1;
+        this.curPage = pageable.getPageNumber() + 1;  // 현제 페이지를 가져옴, 0부터 시작해서 1을 더해준다
+        this.startRow = 1 + (curPage - 1) * perPage;
+        this.endRow = startRow + perPage - 1;
+        this.perPage = pageable.getPageSize();
+
+        int tempEnd = (int)(Math.ceil(curPage / (double) perPagination)) * perPagination;
+
+        start = tempEnd - (perPagination - 1);
+        end = (totalPage > tempEnd) ? tempEnd : totalPage;
+
         prev = start > 1; // 1보다 크면 true, 작으면 false
         next = totalPage > tempEnd;
 
--- src/main/java/idusw/springboot/entity/BaseEntity.java
+++ src/main/java/idusw/springboot/entity/BaseEntity.java
@@ -10,6 +10,14 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+//JPA Auditing 을 위한 공통 추상 클래스
+
+
+//Entity (개체): Persistence - DB 관점 vs 파일 관점
+//개체들 간의 연관 관계: Relationship
+//Service Layer - Repository Layer 사이에서 정보 표현(전달)
+//참고) Domain, DTO 객체: Controller - Service, Controller - View 사이에서 정보 전달
+
 @MappedSuperclass
 @EntityListeners(value = {AuditingEntityListener.class})
 @Getter
